588ef79ac82c2512dc3907a364b22826f7840e65
vuhung512 <59988100+vuhung512@users.noreply.github.com>
2024-04-07 16:00:04 +0700

    Update easybot.user.js
    new up

--- easybot.user.js
+++ easybot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LOKBOT
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  Example Tampermonkey script
 // @author       HUNZU98
 // @match        https://play.leagueofkingdoms.com/*
